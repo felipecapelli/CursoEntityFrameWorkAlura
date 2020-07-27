b00019a4436fcef918463fa37b77b837048fc8e6
felipecapelli <felipe.ccapelli@gmail.com>
2020-07-27 14:55:09 -0300

    Aula 3

--- Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -1,96 +1,68 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.Remoting.Contexts;
 
 namespace Alura.Loja.Testes.ConsoleApp
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            //GravarUsandoAdoNet();
-            //GravarUsandoEntity();
-            //RecuperarProdutos();
-            //ExcluirProdutos();
-            //RecuperarProdutos();
-            //AtualizarProduto();
-
-            Console.ReadLine();
-        }
-
-        private static void AtualizarProduto()
-        {
-            GravarUsandoEntity();
-            RecuperarProdutos();
-
-            using(var repo = new ProdutoDAOEntity())
+        { 
+            using(var contexto = new LojaContext())
             {
-                //Produto primeiro = repo.Produtos.First();
-                primeiro.Nome = "Harry Potter e a Ordem da Fênix - Editado";
-                repo.Atualizar(primeiro);
-            }
-            RecuperarProdutos();
-            
-        }
+                //-----Inicio - Mostra os comandos no banco-------------------
+                var serviceProvider = contexto.GetInfrastructure<IServiceProvider>();
+                var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
+                loggerFactory.AddProvider(SqlLoggerProvider.Create());
+                //-----Fim - Mostra os comandos no banco----------------------
 
-        private static void ExcluirProdutos()
-        {
-            using(var repo = new ProdutoDAOEntity())
-            {
-                IList<Produto> produtos = repo.Produtos();
-                foreach(var item in produtos)
+                var produtos = contexto.Produtos.ToList();
+                
+                ExibeEntries(contexto.ChangeTracker.Entries());
+
+                var novoProduto = new Produto()
                 {
-                    repo.Remover(item);
-                }
+                    Nome = "Sabão em pó",
+                    Categoria = "Limpeza",
+                    Preco = 4.99
+                };
+
+                contexto.Produtos.Add(novoProduto);
+                ExibeEntries(contexto.ChangeTracker.Entries());
+
+                contexto.Remove(novoProduto);
+                //var p1 = produtos.Last();
+                //contexto.Remove(p1);
+                ExibeEntries(contexto.ChangeTracker.Entries());
+
+
+                //contexto.SaveChanges();
+
+                var entry = contexto.Entry(novoProduto);
+                Console.WriteLine("\n\n" + entry.Entity.ToString() + " - " + entry.State);
+
+
+                //ExibeEntries(contexto.ChangeTracker.Entries());
+                //--------------------------------------------------
+                Console.ReadLine();
             }
         }
 
-        private static void RecuperarProdutos()
+        private static void ExibeEntries(IEnumerable<EntityEntry> entries)
         {
-            using(var repo = new ProdutoDAOEntity())
+            Console.WriteLine("---------------------------");
+            //foreach (var e in contexto.ChangeTracker.Entries())
+            foreach (var e in entries)
             {
-                IList<Produto> produtos = repo.Produtos();
-                Console.WriteLine("Foram encontrados {0} produtos(s).", produtos.Count);
-                foreach(var item in produtos)
-                {
-                    Console.WriteLine(item.Nome);
-                }
+                //Console.WriteLine(e);
+                Console.WriteLine(e.Entity.ToString() + " - " + e.State);//Esse e.entity é a referentecia para o produto (no produto tem o To String)
             }
         }
 
-        private static void GravarUsandoEntity()
-        {
-            /*Produto p = new Produto();
-            p.Nome = "Harry Potter e a Ordem da Fênix";
-            p.Categoria = "Livros";
-            p.Preco = 19.89;*/
-
-            Produto p = new Produto();
-            p.Nome = "Cassino Royale";
-            p.Categoria = "Filmes";
-            p.Preco = 19.89;
-
-            using (var contexto = new ProdutoDAOEntity())
-            {
-                contexto.Adicionar(p);
-            }
-        }
-
-
-        //---------------------------------------------------------ADO ANTIGO
-        private static void GravarUsandoAdoNet()
-        {
-            Produto p = new Produto();
-            p.Nome = "Harry Potter e a Ordem da Fênix";
-            p.Categoria = "Livros";
-            p.Preco = 19.89;
-
-            using (var repo = new ProdutoDAO())
-            {
-                repo.Adicionar(p);
-            }
-        }
     }
 }
